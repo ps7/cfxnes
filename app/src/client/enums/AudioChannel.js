411960aaf6b4a8c59ac26b3ffa798f4155c3c87a
jpikl <pikl.jan@gmail.com>
2017-03-01 23:40:04 +0100

    Add missing file

---
+++ app/src/client/enums/AudioChannel.js
@@ -0,0 +1,21 @@
+import {createItems} from './common';
+
+export const MASTER = 'master';
+export const PULSE_1 = 'pulse1';
+export const PULSE_2 = 'pulse2';
+export const TRIANGLE = 'triangle';
+export const NOISE = 'noise';
+export const DMC = 'dmc';
+
+export const values = [MASTER, PULSE_1, PULSE_2, TRIANGLE, NOISE, DMC];
+
+const params = {
+  [MASTER]: {caption: 'Master volume'},
+  [PULSE_1]: {caption: 'Pulse channel 1'},
+  [PULSE_2]: {caption: 'Pulse channel 2'},
+  [TRIANGLE]: {caption: 'Triangle channel'},
+  [NOISE]: {caption: 'Noise channel'},
+  [DMC]: {caption: 'DMC channel'},
+};
+
+export const items = createItems(values, params);
